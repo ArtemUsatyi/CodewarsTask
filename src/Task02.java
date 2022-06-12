8d1667008b2baad0e0b49d5d348ee7a82f553031
artemowork <artem180791@mail.ru>
2022-06-12 16:36:22 +0300

    Проверка палиндрома

---
+++ src/Task02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args){
+        System.out.println("Введите целочисленное число, является ли число Палиндромом:");
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+        int reminder = 0;
+        int a;
+        int sum = inputNumber;
+
+       while (sum>0){
+           a = sum%10;
+           reminder = (reminder*10)+a;
+           sum =sum/10;
+       }
+       if (reminder == inputNumber){
+           System.out.println("Число является палиндромом");
+       }
+       else System.out.println("Число НЕ является палиндромом");
+    }
+}
