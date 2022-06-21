aeecb553241565eb9ec3c635565589f17072acef
Artemo <artemo180791@mail.ru>
2022-06-21 22:00:53 +0300

    Вывести длину последнего слова.

---
+++ src/Task06.java
@@ -0,0 +1,9 @@
+public class Task06 {
+    public static void main(String[] args) {
+       String line = "       text hello sdfdsfsfdsdf dddd ddddddddd";
+       String [] mas = line.split(" ");
+        String text = mas[mas.length-1];
+        int tex = text.length();
+        System.out.println(tex);
+    }
+}
