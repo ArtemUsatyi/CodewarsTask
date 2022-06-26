0ce1d88fde43eb8fa938f28027bfcffc420d14d5
Artemo <artemo180791@mail.ru>
2022-06-26 15:04:58 +0300

    Проверка для отправки

--- src/Task07.java
+++ src/Task07.java
@@ -1,5 +1,10 @@
 public class Task07 {
     public static void main(String[] args) {
-
+    int simple = 2;
+    if (simple%2 == 1){
+        if (simple%3>0){
+            System.out.println("Число " + simple + " простое число");
+        }
+    } else  System.out.println("Число " + simple + " не простое число");
     }
 }
