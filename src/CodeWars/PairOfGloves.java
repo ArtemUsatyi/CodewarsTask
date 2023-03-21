c811a8ed9d2316edda01f7b20527f9d91872c150
Usatyi Artem <artem180791@mail.ru>
2023-03-21 16:36:48 +0300

    Сodewars - найти пару из массива

---
+++ src/CodeWars/PairOfGloves.java
@@ -0,0 +1,25 @@
+package CodeWars;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PairOfGloves {
+    public static void main(String[] args) {
+        String[] gloves = {"1", "1", "2", "2", "4", "2", "3", "4", "2", "3"};
+
+        ArrayList<String> glovesList = new ArrayList<>(Arrays.asList(gloves));
+        int countGloves = 0;
+        for (int i = 1; i < glovesList.size(); i++) {
+            String glove = glovesList.get(0);
+            if (glove.equals(glovesList.get(i))) {
+                countGloves++;
+                glovesList.remove(i);
+                glovesList.remove(glove);
+                i = 0;
+            } else if (glovesList.size() == i) {
+                glovesList.remove(i - 1);
+            }
+        }
+        System.out.println(countGloves);
+    }
+}
