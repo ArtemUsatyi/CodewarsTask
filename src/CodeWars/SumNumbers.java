1f60c4de5805be5080313e18079fc06a459039d7
Usatyi Artem <artem180791@mail.ru>
2023-03-28 14:30:14 +0300

    Сodewars - реализация через BigInteger

--- src/CodeWars/SumNumbers.java
+++ src/CodeWars/SumNumbers.java
@@ -1,11 +1,12 @@
 package CodeWars;
 
-import java.util.OptionalLong;
+import java.math.BigInteger;
+import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 public class SumNumbers {
     public static void main(String[] args) {
-        int value = 3;
+        int value = 100;
 
         int count=0;
         // вычисление 1-ой функции Z(3) = sum(1)+sum(1+2)+sum(1+2+3) Итог: z(3)=10
@@ -13,8 +14,6 @@ public class SumNumbers {
             count += LongStream.rangeClosed(1, i).reduce(Long::sum).getAsLong();
         }
         // вычисление 2-ой функции S(10) = 1+2+3+4+5+6+7+8+9+10
-        OptionalLong countNumber = LongStream.rangeClosed(1, count).reduce((x, y)->x+y);
-        long longNew = countNumber.getAsLong();
-        System.out.println(longNew);
+        System.out.println(IntStream.rangeClosed(1, count).mapToObj(BigInteger::valueOf).reduce(BigInteger::add).get());
     }
 }
