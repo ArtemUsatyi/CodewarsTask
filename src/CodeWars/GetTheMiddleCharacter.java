2e28498529565f5520996cd49b624406e106a8bc
artemowork <artem180791@mail.ru>
2023-04-12 14:57:15 +0300

    Сodewars - получить из середины строки символ. Есть нечетное 1 символ из середины строки, Четное - 2 символа из середины строки!

--- src/CodeWars/GetTheMiddleCharacter.java
+++ src/CodeWars/GetTheMiddleCharacter.java
@@ -4,7 +4,7 @@ public class GetTheMiddleCharacter {
     public static void main(String[] args) {
         String word = "testing";
         int size = word.length();
-        if (size % 2 == 1) System.out.println(word.substring(size / 2,size / 2+1));
+        if (size % 2 == 1) System.out.println(word.charAt(size / 2));
         else System.out.println(word.substring(size / 2 - 1, size / 2) + word.substring(size / 2, size / 2 + 1));
     }
 }
