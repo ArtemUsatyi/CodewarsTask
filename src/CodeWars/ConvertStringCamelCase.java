51f4665394e64fe25e5f6b9c5b358981c0c170de
artemowork <artem180791@mail.ru>
2023-04-13 20:55:04 +0300

    Сodewars - CamelCase строки и исправление ошибки в виде пустой строки.

--- src/CodeWars/ConvertStringCamelCase.java
+++ src/CodeWars/ConvertStringCamelCase.java
@@ -2,10 +2,12 @@ package CodeWars;
 
 public class ConvertStringCamelCase {
     public static void main(String[] args) {
-        String s = "You_have_chosen_to_translate_this_kata_For_your_convenience_we_have_provided_the_existing_test_cases_used_for_the_language_that_you_have_already_completed_as_well_as_all_of_the_other_related_fields";
+        String s = "you_Have_north";
         String[] textArray = s.split("[-_]");
-
-        String newString = textArray[0].substring(0, 1).toLowerCase().concat(textArray[0].substring(1));
+        String newString = "";
+        if (!textArray[0].isEmpty()) {
+            newString = textArray[0].substring(0, 1).concat(textArray[0].substring(1));
+        }
         for (int i = 1; i < textArray.length; i++) {
             newString = newString + textArray[i].substring(0, 1).toUpperCase().concat(textArray[i].substring(1));
         }
