cf4a2f860048c735bd92fc8185eb6875dc180a3c
artemowork <artem180791@mail.ru>
2023-04-30 15:06:49 +0300

    Сodewars - вернуть максимальную сумму подмассива

--- src/CodeWars/MaximumSubarraySum.java
+++ src/CodeWars/MaximumSubarraySum.java
@@ -7,12 +7,7 @@ import static java.util.Comparator.reverseOrder;
 public class MaximumSubarraySum {
     public static void main(String[] args) {
         int[] array = {-2, 1, -3, 4, -1, 2, 1, -5, 4}; // { -2 1 -3 4 -1 2 -5}
-        Set<Integer> setList = Arrays.stream(array).boxed().collect(Collectors.toSet());
-        List<Integer> list = new ArrayList<>();
-        for (int elem : setList) {
-            list.add(elem);
-        }
-        list.sort(reverseOrder());
+        List<Integer> list = Arrays.stream(array).boxed().distinct().sorted(reverseOrder()).collect(Collectors.toList());
         int count = 0;
         for (int i = 0; i < array.length / 2; i++) {
             count += list.get(i);
