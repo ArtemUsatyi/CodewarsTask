8bffa846145412530e361dfeed150536f78d840e
artemowork <artem180791@mail.ru>
2023-05-06 23:45:51 +0300

    Сodewars - отфильтровать список Object чтобы были числа

--- src/CodeWars/ListFilter.java
+++ src/CodeWars/ListFilter.java
@@ -2,17 +2,12 @@ package CodeWars;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class ListFilter {
     public static void main(String[] args) {
         List<Object> list = new ArrayList<>(Arrays.asList(1, 2, "aasf", "aasf", "aasf", "aasf", "d", "13", -123));
-        Iterator<Object> iterator = list.iterator();
-
-        while (iterator.hasNext()) {
-            if (iterator.next() instanceof String) iterator.remove();
-        }
+        list.removeIf(o -> o instanceof String);
         System.out.println(list);
     }
 }
