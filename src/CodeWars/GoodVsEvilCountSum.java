7f9fb35999edf8310d086ea02064cfc8a4ce241e
artemowork <artem180791@mail.ru>
2023-05-07 15:17:03 +0300

    Сodewars - посчитать количество войск Добра и Зла и определить кто победит. Если кол-во одинаковое вернуть НИЧЬЯ

--- src/CodeWars/GoodVsEvilCountSum.java
+++ src/CodeWars/GoodVsEvilCountSum.java
@@ -8,8 +8,7 @@ public class GoodVsEvilCountSum {
         String evilAmounts = "4550 7611 9648 6265 4146 7937 3854";
         int goodValue = Arrays.stream(goodAmounts.split(" ")).mapToInt(Integer::valueOf).sum();
         int evilValue =Arrays.stream(evilAmounts.split(" ")).mapToInt(Integer::valueOf).sum();
-        System.out.println(goodValue);
-        System.out.println(evilValue);
+
         if(goodValue>evilValue) System.out.println("Добро победило");
         else if (evilValue>goodValue) System.out.println("Зло победило");
         else System.out.println("Ничья");
