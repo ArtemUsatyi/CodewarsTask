7fe42541f1d74862ea652dd8c6e324d77dc56ac1
artemowork <artem180791@mail.ru>
2023-05-15 16:25:20 +0300

    Сodewars - есть массив и надо вернуть массив сумм его частей, как определено: newArray = [0, 1, 3, 6, 10] newArray = [1, 3, 6, 10] newArray = [3, 6, 10] newArray = [6, 10] newArray = [10] newArray = [] Исходный массив : в списке должно быть определенное количество повторений цифр

---
+++ src/CodeWars/DeleteOccurrencesOfElement.java
@@ -0,0 +1,24 @@
+package CodeWars;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DeleteOccurrencesOfElement {
+    public static void main(String[] args) {
+        int[] array = {20, 37, 20, 21};
+        int maxRepeat = 1;
+        List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList());
+        List<Integer> newList = new ArrayList<>();
+
+        for (Integer elem : list) {
+            int count = (int) newList.stream().filter(i -> i == elem).count();
+            if (count < maxRepeat) newList.add(elem);
+        }
+        int[] newArray = newList.stream().mapToInt(i -> i).toArray();
+
+        System.out.println(Arrays.toString(newArray));
+
+    }
+}
