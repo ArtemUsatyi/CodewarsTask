899636ded4d6172ea458e3294b139a188d6dff13
artemowork <artem180791@mail.ru>
2023-05-15 16:28:17 +0300

    Сodewars - вернуть массив, где количество повторений ограничено лимитом

--- src/CodeWars/DeleteOccurrencesOfElement.java
+++ src/CodeWars/DeleteOccurrencesOfElement.java
@@ -9,15 +9,14 @@ public class DeleteOccurrencesOfElement {
     public static void main(String[] args) {
         int[] array = {20, 37, 20, 21};
         int maxRepeat = 1;
-        List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList());
+        //List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList());
         List<Integer> newList = new ArrayList<>();
 
-        for (Integer elem : list) {
+        for (int elem : array) {
             int count = (int) newList.stream().filter(i -> i == elem).count();
             if (count < maxRepeat) newList.add(elem);
         }
         int[] newArray = newList.stream().mapToInt(i -> i).toArray();
-
         System.out.println(Arrays.toString(newArray));
 
     }
