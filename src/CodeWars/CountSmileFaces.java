09212d745a413fc01900ce64438e6527f3d7394c
artemowork <artem180791@mail.ru>
2023-05-16 15:14:57 +0300

    Сodewars - посчитать в массиве количество символов. ОБЯЗАТЕЛЬНО учитывать ГЛАЗА : ; РОТ ) D НОС (не всегда) - ~. Другие символы игнорировать.

---
+++ src/CodeWars/CountSmileFaces.java
@@ -0,0 +1,23 @@
+package CodeWars;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CountSmileFaces {
+    public static void main(String[] args) {
+        List<String> listSmileFaces = Arrays.asList(":-)", "-(", ";-D", ":-D", ";-(");
+        // Смайл должен обязательно иметь
+        // ГЛАЗА : ;
+        // РОТ ) D
+        // НОС (не всегда) - ~
+        int count = 0;
+        for (String elem : listSmileFaces) {
+            if (elem.substring(0, 1).matches("\\:|\\;") && elem.substring(1, 2).matches("\\-|\\~")
+                    && elem.substring(2).matches("\\)||D")) count++;
+            else if (elem.substring(0, 1).matches("\\:|\\;") && elem.substring(1).matches("\\)||D")) count++;
+        }
+        System.out.println(count);
+
+    }
+}
