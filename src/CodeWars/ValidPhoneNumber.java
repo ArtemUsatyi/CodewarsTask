042d6d8cae434a310d6a61c9f3176c28d1a1c383
artemowork <artem180791@mail.ru>
2023-05-27 15:13:15 +0300

    Сodewars - Валидность номера телефона. "(123) 456-7890" --- true. Другие варианты false

--- src/CodeWars/ValidPhoneNumber.java
+++ src/CodeWars/ValidPhoneNumber.java
@@ -3,7 +3,7 @@ package CodeWars;
 public class ValidPhoneNumber {
     public static void main(String[] args) {
         String numberPhone = "(123) 456-7890";
-        int i = 0;
-        if (numberPhone.matches("[(]\\d{3}[)] \\d{3}-\\d{4}")) i = 1;
+        if (numberPhone.matches("[(]\\d{3}[)] \\d{3}-\\d{4}")) System.out.println("Действительный номер телефона");
+        else System.out.println("Не действительный номер телефона");
     }
 }
