8867ee485f0c42369fd382bb3cb684b781fff7eb
artemowork <artem180791@mail.ru>
2023-12-13 17:45:45 +0300

    Дан массив строк, в строке удалить повторяющие рядом буквы

---
+++ src/CodeWars/StringArrayDuplicates.java
@@ -0,0 +1,17 @@
+package CodeWars;
+
+import java.util.Arrays;
+
+public class StringArrayDuplicates {
+    public static void main(String[] args) {
+        String[] arr = {"abracadabra", "allottee", "assessee"};
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].replaceAll("([a-z])\\1+", "$1");
+        }
+        System.out.println(Arrays.toString(arr));
+        
+// Реализация через стрим
+// return Arrays.stream(arr).map(x->x.replaceAll("(.)(?=\\1)","")).toArray(String[]::new);
+    }
+}
