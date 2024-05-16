e485c486738d6082b7b2c0872811e29fd14bd10e
artemowork <artem180791@mail.ru>
2024-05-16 23:06:31 +0300

    еще одно решение палиндрома строки через цикл while

--- src/CodeWars/PalindromeString.java
+++ src/CodeWars/PalindromeString.java
@@ -3,13 +3,25 @@ package CodeWars;
 public class PalindromeString {
 
     public static void main(String[] args) {
-        String word = "p1appap123-=.";
+        String word = "p1appap123-=.qwe";
         String result = word.replaceAll("[^a-zA-Z]", "");
 
         String text = new StringBuilder(result).reverse().toString();
 
-        if (result.equals(text)){
+        if (result.equals(text)) {
             System.out.println("Правда");
         } else System.out.println("Ложь");
+
+        int start = 0;
+        int end = result.length() - 1;
+        while (start < end) {
+            if (result.charAt(start) != result.charAt(end)) {
+                System.out.println("Ложь");
+                break;
+            }
+            start++;
+            end--;
+        }
+        System.out.println("Правда");
     }
 }
